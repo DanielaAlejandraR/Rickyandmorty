0eaf382157803dc8caa5a858f045af0d126de2d1
AlejandraRiaño <aleja98riano@gmail.com>
2023-07-19 09:27:00 -0500

    AGREGADO ADD RANDOM

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,16 +2,22 @@ import SearchBar  from "../SearchBar/SearchBar.jsx";
 import { Link } from "react-router-dom";
 import style from "./Nav.module.css"
 
-const  Nav = ({onSearch}) => {
+const  Nav = ({onSearch, random}) => {
 
     return (
         <div>
+            <SearchBar onSearch={onSearch}/>  
+
             <button className={style.Button}> 
                  <Link to="/home">Home</Link></button>    
             <button className={style.Button}>
                 <Link to ="/about">About</Link> </button>   
-                
-                < SearchBar onSearch={onSearch}/>  
+
+            <button onClick={random}>ADD RANDOM</button>
+
+             <img className={style.img} src='https://1.bp.blogspot.com/-AvVxtmDEbCw/YRQjDiXFhCI/AAAAAAABAR8/lNQZ_fg-C9UXdoNkqJ3uVOwANlu7S_ZAACLcBGAsYHQ/s3200/tipografia-rick-y-morty.jpg' width='20%'/>
+
+            
         </div>
     )
 }
